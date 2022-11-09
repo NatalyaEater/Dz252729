24aeba09aae7dbf5aa4c10763fe5ad3faa31c562
Natasha Eater <natalya.iter@yandex.ru>
2022-11-09 19:34:48 +0300

    3

---
+++ Zadacha3/ZD29.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.(в примере показаны два разных массива, создаем именно для 8 элементов)
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+Console.WriteLine("Задайте длинну массива");
+int  numberLength= int.Parse(Console.ReadLine() ?? "0");
+int []array=new int[numberLength];
+arrayRandom(array);
+
+//функция
+int[] arrayRandom (int []array)
+{
+for (int i=0; i<array.Length;i++)
+{
+    array[i]=new Random().Next(0,9);
+Console.Write(array[i]+" ");
+}
+return array;
+}
