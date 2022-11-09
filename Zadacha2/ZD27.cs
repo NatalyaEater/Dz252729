588c9783966c1631a8e5f16e7d344d198726ce02
Natasha Eater <natalya.iter@yandex.ru>
2022-11-09 18:48:30 +0300

    one2

---
+++ Zadacha2/ZD27.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Clear();
+Console.WriteLine("Введите число  ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int sum2=SumNumber(number);
+Console.WriteLine(sum2);
+
+//функция
+int SumNumber(int randomnum)
+{
+ int i = 0;
+ int sum=0;
+ int ost=0;
+ while (randomnum >= i)
+    {
+ ost=randomnum%10;
+    sum=sum+ost;
+    randomnum=randomnum/10;
+ i=i+1;
+    }
+ return sum;
+}
